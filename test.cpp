c24ac19bb4b3ef6562a9009e47b62c8ac388e54e
Geek-Deer <geekdeer@gmail.com>
2024-08-29 11:59:59 -0600

    First commit!

---
+++ test.cpp
@@ -0,0 +1,18 @@
+/*
+    Nombre: Diego Sánchez Luna
+    Matrícula: A00227831
+*/
+
+// Libs
+#include <iostream>
+using namespace std;
+
+// Funciones
+
+// Variables
+int main()
+{
+    
+    cout << "Hello, World!" << endl;
+    return 0;
+}
